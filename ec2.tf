7cae753af16997ce3843825858f39339d5b972ec
Rajendran88 <157369546+Rajendran88@users.noreply.github.com>
2024-02-14 14:48:22 +0100

    Update ec2.tf
    terraform updated

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "instance" {
 ami = "ami-0d442a425e2e0a743"
 instance_type = "t3.micro"
-availability_zone = "us-west-2c"
+availability_zone = "us-west-2a"
 associate_public_ip_address = true
 key_name = "vockey"
-vpc_security_group_ids = ["sg-0474c38a77b7cfdbe"]
-subnet_id = "subnet-0afa1c8e7708ded4a"
+vpc_security_group_ids = ["sg-069bd1de910a21138"]
+subnet_id = "subnet-0f7a0baa9ab24bc7b"
 # iam_instance_profile = "LabRole”
 count = 1
 tags = {
