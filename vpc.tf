405a0f7dc71424d80f811ce8a7a5dd4115729992
“subashree“ <subashree.9@gmail.com>
2024-02-23 15:47:00 +0100

    update rtb

--- vpc.tf
+++ vpc.tf
@@ -80,5 +80,5 @@ resource "aws_route_table_association" "my_publicSubnetAssociation1" {
 resource "aws_route_table_association" "my_publicSubnetAssociation2" {
     route_table_id = aws_route_table.my_publicRouteTable1.id
     subnet_id = aws_subnet.my_publicsubnet2.id
-    depends_on = [aws_route_table.nf_publicRouteTable1, aws_subnet.my_publicsubnet2]
+    depends_on = [aws_route_table.my_publicRouteTable1, aws_subnet.my_publicsubnet2]
 }
