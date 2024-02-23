7c93ae2bbd0af537610a921fb4d055275cde012f
“subashree“ <subashree.9@gmail.com>
2024-02-23 15:43:58 +0100

    new update

--- vpc.tf
+++ vpc.tf
@@ -54,4 +54,31 @@ resource "aws_internet_gateway" "my_IGW"{
     tags = {
         Name = "myvpcigw"
     }
+} 
+# Routing tables
+
+# Provides a resource to create a VPC routing table
+resource "aws_route_table" "my_publicRouteTable1"{
+    vpc_id = aws_vpc.dev_vpc.id
+    route{
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my_IGW.id
+    }
+    tags = {
+        Name = "my_publicRoute1"
+    }
+}
+
+# Provides a resource to create an association between a Public Route Table and a Public Subnet
+resource "aws_route_table_association" "my_publicSubnetAssociation1" {
+    route_table_id = aws_route_table.my_publicRouteTable1.id
+    subnet_id = aws_subnet.my_publicsubnet1.id
+    depends_on = [aws_route_table.my_publicRouteTable1, aws_subnet.my_publicsubnet1]
+}
+
+# Provides a resource to create an association between a Public Route Table and a Public Subnet
+resource "aws_route_table_association" "my_publicSubnetAssociation2" {
+    route_table_id = aws_route_table.my_publicRouteTable1.id
+    subnet_id = aws_subnet.my_publicsubnet2.id
+    depends_on = [aws_route_table.nf_publicRouteTable1, aws_subnet.my_publicsubnet2]
 }
