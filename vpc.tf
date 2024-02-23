36cb8888f68753d21607e217f4794bd35afad254
“subashree“ <subashree.9@gmail.com>
2024-02-23 14:35:58 +0100

    modified cidr

--- vpc.tf
+++ vpc.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "public-1" {
 }
 resource "aws_subnet" "private-1" {
   vpc_id     = aws_vpc.dev_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-west-2a"
   map_public_ip_on_launch = true
 
@@ -30,7 +30,7 @@ resource "aws_subnet" "private-1" {
 }
 resource "aws_subnet" "public-2" {
   vpc_id     = aws_vpc.dev_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.3.0/24"
   availability_zone = "us-west-2b"
   map_public_ip_on_launch = true
 
@@ -40,7 +40,7 @@ resource "aws_subnet" "public-2" {
 }
 resource "aws_subnet" "private-2" {
   vpc_id     = aws_vpc.dev_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.4.0/24"
   availability_zone = "us-west-2b"
   map_public_ip_on_launch = true
 
