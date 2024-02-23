6bd0b8b6a10d9323469c1a8efa6be6a502102df3
“subashree“ <subashree.9@gmail.com>
2024-02-23 13:50:06 +0100

    ec2 update

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "instance" {
-ami = "ami-0d442a425e2e0a743"
-instance_type = "t3.micro"
-availability_zone = "us-west-2a"
-associate_public_ip_address = true
-key_name = "vockey"
-vpc_security_group_ids = ["sg-04a3003e0b9b4bb3c"]
-subnet_id = "subnet-088e29de59fac6391"
-# iam_instance_profile = "LabRole”
-count = 1
-tags = {
-Name = "Sandbox1"
-}
-}
+# resource "aws_instance" "instance" {
+# ami = "ami-0d442a425e2e0a743"
+# instance_type = "t3.micro"
+# availability_zone = "us-west-2a"
+# associate_public_ip_address = true
+# key_name = "vockey"
+# vpc_security_group_ids = ["sg-04a3003e0b9b4bb3c"]
+# subnet_id = "subnet-088e29de59fac6391"
+# # iam_instance_profile = "LabRole”
+# count = 1
+# tags = {
+# Name = "Sandbox1"
+# }
+# }
