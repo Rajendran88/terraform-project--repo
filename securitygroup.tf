11015b5be4801b8f7556c3849ced5d9cc25b5a44
“subashree“ <subashree.9@gmail.com>
2024-02-24 15:29:16 +0100

    modified security group

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,35 +1,114 @@
-# Security Groups
-resource "aws_security_group" "my_sg_http"{
-    vpc_id = aws_vpc.dev_vpc.id
-    name = "my_securityGrouplatest"
-    tags = {
-        Name = "my_sg_http"
-    }     
+
+ # Create a Security Group for the VPC
+resource "aws_security_group" "sg_vpc" {
+  name        = "sg_vpc"
+  description = "allow shh"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  # Add inbound rules
+  # Add a rule for HTTP
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.CIDR_BLOCK]
+  }
+
+  # Add a rule for HTTPS
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.CIDR_BLOCK]
+  }
+
+  # Add a rule for SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.CIDR_BLOCK]
+  }
+
+  # Add an outbound rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.CIDR_BLOCK]
+  }
+  tags = {
+    Name = "sg-vpc"
+  }
 }
-# Ingress Security Port 80 (Inbound)
-resource "aws_security_group_rule" "my_ingress_http"{
-    from_port = 80
-    protocol = "tcp"
-    security_group_id = aws_security_group.my_sg_http.id
-    to_port= 80
-    type = "ingress"
-    cidr_blocks = ["0.0.0.0/0"] # <Specify the CIDR> instead of allowing it to public
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH traffic"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  ingress {
+    description      = "allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
-# Ingress Security Port 443 (Inbound)
-resource "aws_security_group_rule" "my_ingress_https"{
-    from_port = 443
-    protocol = "tcp"
-    security_group_id = aws_security_group.my_sg_http.id
-    to_port= 443
-    type = "ingress"
-    cidr_blocks = ["0.0.0.0/0"] # <Specify the CIDR> instead of allowing it to public
+
+resource "aws_security_group" "allow_ec2_aurora" {
+  name        = "allow_ec2_aurora"
+  description = "Allow EC2 to Aurora traffic"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  ingress {
+    description      = "allow ec2 to aurora"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
-# Allow Access All (Outbound)
-resource "aws_security_group_rule" "my_outbound_all"{
-    from_port = 0
-    protocol = "-1"
-    security_group_id = aws_security_group.my_sg_http.id
-    to_port= 0
-    type = "egress"
+
+resource "aws_security_group" "allow_aurora_access" {
+  name        = "allow_aurora_access"
+  description = "Allow EC2 to aurora"
+  vpc_id = aws_vpc.dev_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    # security_groups = [aws_security_group.allow_ssh.id] 
+  }
+
+  tags = {
+    Name = "aurora-stack-allow-aurora-MySQL"
+  }
 }
