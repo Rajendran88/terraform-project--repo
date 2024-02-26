725a44eb8fe9bb4db7d241184a12331535ae8cc2
“subashree“ <subashree.9@gmail.com>
2024-02-26 12:52:55 +0100

    update 13 ec2

--- ec2project.tf
+++ ec2project.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids      = [aws_security_group.sg_vpc.id]
   subnet_id                   = aws_subnet.public-1.id
 
-  iam_instance_profile        = "Labrole"
+  #iam_instance_profile        = "Labrole"
   count = 1
   tags = {
     Name = "Sandbox1"
